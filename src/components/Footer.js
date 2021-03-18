fbf49b5074d8e0f2350c1e9548b470d0aada3166
PamelaPz <jpamelapz02@gmail.com>
2021-03-18 14:56:52 -0600

    footer terminado

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+function Footer() {
+
+    return (
+        <div className="footer_wrapper">
+            <ul className="list_footer">
+                <li>© 2021 Tooring</li>
+                <li>
+                    <div className="redes_content">
+                        <a href="https://www.facebook.com/TooringMX"><FontAwesomeIcon className="text-blak" icon={faFacebook} size="lg" /></a>
+                        <a href="https://www.instagram.com/tooringmx/"><FontAwesomeIcon className="text-blak" icon={faInstagram} size="lg" /></a>
+                        <a href="#"><FontAwesomeIcon className="text-blak" icon={faLinkedin} size="lg" /></a>
+                    </div>
+                </li>
+                <li>Snisi sem lobortis</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Footer;
